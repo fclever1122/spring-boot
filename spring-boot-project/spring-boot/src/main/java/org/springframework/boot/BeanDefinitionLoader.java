b39228805c8cacb963e1fc5b1fa9e7dec87a0072
fclever1122 <1393906015@qq.com>
2023-06-20 22:20:59 +0800

    8-SpringBoot启动过程中自动装配(补充）

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/BeanDefinitionLoader.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/BeanDefinitionLoader.java
@@ -133,16 +133,20 @@ class BeanDefinitionLoader {
 	private int load(Object source) {
 		// 根据资源的类型不同来执行不同的load方法
 		Assert.notNull(source, "Source must not be null");
+		// 如果是class类型，启用注解类型
 		if (source instanceof Class<?>) {
 			// SpringBoot启动类属于class
 			return load((Class<?>) source);
 		}
+		// 如果是resource类型，启用xml解析
 		if (source instanceof Resource) {
 			return load((Resource) source);
 		}
+		// 如果是package类型，启用扫描包，例如@ComponentScan
 		if (source instanceof Package) {
 			return load((Package) source);
 		}
+		// 如果是字符串类型，直接加载
 		if (source instanceof CharSequence) {
 			return load((CharSequence) source);
 		}
